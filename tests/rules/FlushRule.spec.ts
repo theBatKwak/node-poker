ef88b578d5f82a8c6d2673b92b86ad7665637238
Robin OGER <oger.robin@gmail.com>
2022-08-10 23:55:18 +0200

    fix: flush rule typo

--- tests/rules/FlushRule.spec.ts
+++ tests/rules/FlushRule.spec.ts
@@ -3,7 +3,7 @@ import { FlushRule } from '../../services/rules/FlushRule.service'
 
 describe('FlushRule', () => {
     it('should return false if less than 5 cards are provided', () => {
-        const straightRule = new FlushRule()
+        const flushRule = new FlushRule()
         const cards = [
             new Card('clubs', '10', 10),
             new Card('clubs', 'J', 11)
@@ -12,11 +12,11 @@ describe('FlushRule', () => {
             new Card('clubs', '3', 3),
             new Card('clubs', '5', 5)
         ]
-        const result = straightRule.is([cards[0], cards[1]], [cards[2], cards[3]])
+        const result = flushRule.is([cards[0], cards[1]], [cards[2], cards[3]])
         expect(result).toBe(false)
     }) 
     it('should return a correct flush when 5 cards with flush is provided', () => {
-        const straightRule = new FlushRule()
+        const flushRule = new FlushRule()
         const cards = [
             new Card('clubs', '10', 10),
             new Card('clubs', 'A', 14)
@@ -26,11 +26,11 @@ describe('FlushRule', () => {
             new Card('clubs', '5', 5),
             new Card('clubs', 'J', 11)
         ]
-        const result = straightRule.is(cards, commonCards)
+        const result = flushRule.is(cards, commonCards)
         expect(result).toStrictEqual({ value: 14, flush: 'clubs' })
     })
     it('should return false when 5 cards with no flush cards is provided', () => {
-        const straightRule = new FlushRule()
+        const flushRule = new FlushRule()
         const cards = [
             new Card('clubs', '10', 10),
             new Card('clubs', 'J', 11)
@@ -40,11 +40,11 @@ describe('FlushRule', () => {
             new Card('clubs', '5', 5),
             new Card('hearts', 'A', 14)
         ]
-        const result = straightRule.is(cards, commonCards)
+        const result = flushRule.is(cards, commonCards)
         expect(result).toBe(false)
     })
     it('should return a correct flush when 6 cards with flush is provided', () => {
-        const straightRule = new FlushRule()
+        const flushRule = new FlushRule()
         const cards = [
             new Card('clubs', '10', 10),
             new Card('clubs', 'Q', 12)
@@ -55,11 +55,11 @@ describe('FlushRule', () => {
             new Card('clubs', 'J', 11),
             new Card('hearts', 'J', 11)
         ]
-        const result = straightRule.is(cards, commonCards)
+        const result = flushRule.is(cards, commonCards)
         expect(result).toStrictEqual({ value: 12, flush: 'clubs' })
     })
     it('should return false when 6 cards with no flush cards is provided', () => {
-        const straightRule = new FlushRule()
+        const flushRule = new FlushRule()
         const cards = [
             new Card('clubs', '10', 10),
             new Card('clubs', 'J', 11)
@@ -70,11 +70,11 @@ describe('FlushRule', () => {
             new Card('hearts', 'A', 14),
             new Card('hearts', '2', 2)
         ]
-        const result = straightRule.is(cards, commonCards)
+        const result = flushRule.is(cards, commonCards)
         expect(result).toBe(false)
     })
     it('should return a correct flush when 7 cards with flush is provided', () => {
-        const straightRule = new FlushRule()
+        const flushRule = new FlushRule()
         const cards = [
             new Card('clubs', '10', 10),
             new Card('clubs', 'Q', 12)
@@ -86,11 +86,11 @@ describe('FlushRule', () => {
             new Card('clubs', 'A', 14),
             new Card('hearts', 'J', 11)
         ]
-        const result = straightRule.is(cards, commonCards)
+        const result = flushRule.is(cards, commonCards)
         expect(result).toStrictEqual({ value: 14, flush: 'clubs' })
     })
     it('should return false when 7 cards with no flush cards is provided', () => {
-        const straightRule = new FlushRule()
+        const flushRule = new FlushRule()
         const cards = [
             new Card('clubs', '10', 10),
             new Card('clubs', 'J', 11)
@@ -102,7 +102,7 @@ describe('FlushRule', () => {
             new Card('spades', 'A', 14),
             new Card('hearts', '2', 2)
         ]
-        const result = straightRule.is(cards, commonCards)
+        const result = flushRule.is(cards, commonCards)
         expect(result).toBe(false)
     })
 })

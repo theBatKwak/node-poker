f13c7f3639b98a7e1e05e197facf73808013b25c
Robin OGER <oger.robin@gmail.com>
2022-11-19 16:10:59 +0100

    feat: full house score

--- services/Score.service.ts
+++ services/Score.service.ts
@@ -4,6 +4,7 @@ import { PokerRuleResult } from './rules/PokerRule.service'
 export class ScoreService {
   public static getScore(ruleResult: PokerRuleResult, userCards: Card[], commonCards: Card[]): number {
     let value = '0'
+    let secondValue = '00'
     switch (ruleResult.reason) {
       case 'royal flush':
         return 81400000000
@@ -12,8 +13,12 @@ export class ScoreService {
         return parseInt(8 + value + '00000000')
       case 'four of a kind':
         value = this.getStringFromValue(ruleResult.value)
-        const secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
+        secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
         return parseInt(7 + value + secondValue + '000000')
+      case 'full house':
+        value = this.getStringFromValue(ruleResult.value)
+        secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
+        return parseInt(6 + value + value + value + secondValue + secondValue)
     }
     return 0
   }
--- tests/score/FullHouseScore.spec.ts
+++ tests/score/FullHouseScore.spec.ts
@@ -2,31 +2,31 @@ import { ScoreService } from '../../services/Score.service'
 import { Card } from '../../models/Card'
 import { PokerRule } from '../../services/rules/PokerRule.service'
 
-describe('FourOfAKindScore', () => {
-  it('should return a correct four of a kind score with 2 digits best card', () => {
+describe('FullHouseScore', () => {
+  it('should return a correct full house score with 2 digits best card', () => {
     const userCards = [new Card('diamonds', 'A', 14), new Card('hearts', 'A', 14)]
     const commonCards = [
       new Card('clubs', 'A', 14),
-      new Card('spades', 'A', 14),
+      new Card('spades', 'K', 13),
       new Card('spades', 'K', 13),
       new Card('clubs', '7', 7),
       new Card('diamonds', '6', 6)
     ]
     const result = new PokerRule().have(userCards, commonCards)
     const score = ScoreService.getScore(result, userCards, commonCards)
-    expect(score).toBe(71413000000)
+    expect(score).toBe(61414141313)
   })
-  it('should return a correct four of a kind score with 1 digits best card', () => {
+  it('should return a correct full house score with 1 digits best card', () => {
     const userCards = [new Card('diamonds', '2', 2), new Card('hearts', '2', 2)]
     const commonCards = [
       new Card('clubs', 'A', 14),
-      new Card('spades', 'A', 14),
-      new Card('spades', '2', 2),
+      new Card('spades', '3', 3),
+      new Card('spades', '3', 3),
       new Card('clubs', '2', 2),
       new Card('diamonds', '6', 6)
     ]
     const result = new PokerRule().have(userCards, commonCards)
     const score = ScoreService.getScore(result, userCards, commonCards)
-    expect(score).toBe(70214000000)
+    expect(score).toBe(60202020303)
   })
 })

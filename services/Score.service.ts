6c41d993ea9caff00408fa3dd8f7ba5c26d5aab7
Robin OGER <oger.robin@gmail.com>
2022-11-19 17:44:30 +0100

    feat: score service refacto

--- services/Score.service.ts
+++ services/Score.service.ts
@@ -3,45 +3,27 @@ import { PokerRuleResult } from './rules/PokerRule.service'
 
 export class ScoreService {
   public static getScore(ruleResult: PokerRuleResult, userCards: Card[], commonCards: Card[]): number {
-    let value = '0'
-    let secondValue = '00'
-    let sideKick = '00'
+    let value = this.getStringFromValue(ruleResult.value)
+    let secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
+    let sideKick = ruleResult.sideKick ? this.getStringFromValue(ruleResult.sideKick) : '00'
     switch (ruleResult.reason) {
       case 'royal flush':
         return 81400000000
       case 'straight flush':
-        value = this.getStringFromValue(ruleResult.value)
         return parseInt(8 + value + '00000000')
       case 'four of a kind':
-        value = this.getStringFromValue(ruleResult.value)
-        secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
         return parseInt(7 + value + secondValue + '000000')
       case 'full house':
-        value = this.getStringFromValue(ruleResult.value)
-        secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
         return parseInt(6 + value + value + value + secondValue + secondValue)
       case 'flush':
-        value = this.getStringFromValue(ruleResult.value)
-        secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
-        sideKick = ruleResult.sideKick ? this.getStringFromValue(ruleResult.sideKick) : '00'
         return parseInt(5 + value + secondValue + sideKick + '0000')
       case 'straight':
-        value = this.getStringFromValue(ruleResult.value)
         return parseInt(4 + value + '00000000')
       case 'three of a kind':
-        value = this.getStringFromValue(ruleResult.value)
-        secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
-        sideKick = ruleResult.sideKick ? this.getStringFromValue(ruleResult.sideKick) : '00'
         return parseInt(3 + value + value + value + secondValue + sideKick)
       case 'two pairs':
-        value = this.getStringFromValue(ruleResult.value)
-        secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
-        sideKick = ruleResult.sideKick ? this.getStringFromValue(ruleResult.sideKick) : '00'
         return parseInt(2 + value + value + secondValue + secondValue + sideKick)
       case 'one pair':
-        value = this.getStringFromValue(ruleResult.value)
-        secondValue = ruleResult.secondValue ? this.getStringFromValue(ruleResult.secondValue) : '00'
-        sideKick = ruleResult.sideKick ? this.getStringFromValue(ruleResult.sideKick) : '00'
         return parseInt(1 + value + value + secondValue + sideKick + '00')
     }
     return 0
